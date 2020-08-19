705085a01f04149b244b89858a2c94dae1843031
Milosm991 <milosmanojlovic91@gmail.com>
2020-08-19 18:36:07 +0200

    Updates

---
+++ precache-manifest.36bde4cce2b7bfec8f4c363352381db8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9442bd8ba36bcf644467aaed833fbd7",
+    "url": "/tv-shows/index.html"
+  },
+  {
+    "revision": "e05bc5ded14e29ca1d17",
+    "url": "/tv-shows/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e8aa2102c541e31deff0",
+    "url": "/tv-shows/static/css/main.ddde8f80.chunk.css"
+  },
+  {
+    "revision": "e05bc5ded14e29ca1d17",
+    "url": "/tv-shows/static/js/2.abc32c8e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/tv-shows/static/js/2.abc32c8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8aa2102c541e31deff0",
+    "url": "/tv-shows/static/js/main.8277d883.chunk.js"
+  },
+  {
+    "revision": "ff8400bef69e3d63a559",
+    "url": "/tv-shows/static/js/runtime-main.e51d93e4.js"
+  }
+]);
